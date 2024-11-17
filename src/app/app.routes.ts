aeeeaabd44aec908b48d2fe9ba65c2f65d0c55df
Brandon Presley <brandon@presley.nz>
2024-11-17 17:26:37 +1300

    Add lesson1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Lesson1Component } from './lesson1/lesson1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Lesson1Component }];
