cc4be5159ce94ca7f6b0c4941927fee700e4c373
Brandon Presley <brandon@presley.nz>
2024-11-18 11:38:25 +1300

    Add lessons 8 & 9
    Lesson 10 will be mainly a
    user assessment to create
    a TODO page.

--- src/app/lesson8/lesson8.component.ts
+++ src/app/lesson8/lesson8.component.ts
@@ -1,12 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+interface Post {
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-lesson8',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './lesson8.component.html',
-  styleUrl: './lesson8.component.css'
+  styleUrl: './lesson8.component.css',
 })
 export class Lesson8Component {
+  constructor(private http: HttpClient) {}
 
+  posts: Post[] = [];
+  loading = false;
+
+  getPosts() {
+    this.loading = true;
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((value: any) => {
+        this.loading = false;
+        return (this.posts = value);
+      });
+  }
 }
--- src/app/lesson9/lesson9.component.ts
+++ src/app/lesson9/lesson9.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lesson9',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lesson9.component.html',
-  styleUrl: './lesson9.component.css'
+  styleUrl: './lesson9.component.css',
 })
 export class Lesson9Component {
-
+  title = 'Pipes';
+  now = new Date();
+  username = 'CamelJoe123';
+  sentence = 'this sentence isnt formatted for titles';
 }
